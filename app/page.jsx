d48f6aa46f08d65f1a92c3f3ec2d1ac8b9533a6d
kyryloren <dev@kyryloorlov.com>
2024-11-18 02:30:17 -0500

    init

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+'use client'
+
+import Gallery from '@/components/gallery'
+// import Carousel from '@/components/canvas/carousel'
+
+export default function Page() {
+  return <></>
+}
---
+++ src/components/canvas/carousel/post.jsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { forwardRef } from 'react'
+import { useThree } from '@react-three/fiber'
+import { MeshTransmissionMaterial } from '@react-three/drei'
+import { Color } from 'three'
+
+const PostProcessing = forwardRef((_, ref) => {
+  const { viewport } = useThree()
+
+  return (
+    <mesh position={[0, 0, 1]}>
+      <planeGeometry args={[viewport.width, viewport.height]} />
+      <MeshTransmissionMaterial
+        ref={ref}
+        background={new Color('black')}
+        transmission={0.7}
+        roughness={0}
+        thickness={0}
+        chromaticAberration={0.06}
+        anisotropy={0}
+        ior={1.2}
+      />
+    </mesh>
+  )
+})
+
+export default PostProcessing
